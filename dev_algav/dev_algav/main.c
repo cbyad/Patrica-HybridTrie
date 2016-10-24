e795ba22e9bbcfffdb25149c12fbad9164891bf1
cbyad <cb_mac@cbmac.wifi.crans.org>
2016-10-24 12:13:22 +0200

    first commit test

---
+++ dev_algav/dev_algav/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dev_algav
+//
+//  Created by CB_MAC on 24/10/2016.
+//  Copyright © 2016 CB_MAC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

b2ed63d2436e759f4bc89d39d515de879f34f5ee
cbyad <cb_mac@cbmac.wifi.crans.org>
2016-11-13 21:01:57 +0100

    importation des mots depuis fichier vers la liste PS: revoir la fonction d'affichage de la liste printList()

--- src/build_dict.c
+++ src/build_dict.c
@@ -1,2 +1,35 @@
 #include "../include/build_dict.h"
 
+#define SIZE_MAX 30
+
+
+
+wlist read_dict(char* path){
+    
+    wlist myList =NULL ;
+    
+    char stringContent[SIZE_MAX];
+    
+    FILE* file=fopen(path,"r");
+    
+    if (file != NULL)
+    {
+        
+        rewind(file); // put cursor on the begin of the file
+        myList = newList();
+       
+            while (fgets(stringContent,SIZE_MAX, file) != NULL)
+            {
+                addList(myList, stringContent);
+            }
+    
+            fclose(file);
+            return myList ;
+    
+    }
+    else
+    {
+        return NULL ;
+    }
+
+}

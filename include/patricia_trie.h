b93e56a27ecd04bd49574ea0f879031d6ad513e8
cbyad <cb_mac@cbmac.wifi.crans.org>
2016-11-14 02:36:25 +0100

    Definition provisoire des prototypes des fonctions prochaine Mise a jour de la structure Patricia et adaptation aux prototypes

--- include/patricia_trie.h
+++ include/patricia_trie.h
@@ -1,5 +1,7 @@
 #ifndef _PATRICIA_TRIE_
 #define _PATRICIA_TRIE_
+#include <stdbool.h>
+#include "word_list.h"
 
 struct patricia_node {
   char* val;
@@ -7,6 +9,55 @@ struct patricia_node {
   struct patricia_node* sibling;
 };
 typedef struct patricia_node* patricia_node;
+
+
+/****************
+ * PRIMITIVES
+ ****************/
+
+
+// Create a node with key as a key
+patricia_node creerPatriciaVide(char* word);
+
+// Insert a new word to a Patricia trie
+patricia_node ajouterMot(patricia_node t ,char* mot);
+
+// Remove a patricia trie from memory
+void libererPT(patricia_node t);
+
+
+
+/**********************
+ * ADVANCES FUNCTIONS
+ **********************/
+
+// Search a word
+bool recherche(patricia_node t ,char* mot);
+
+// Count words in patricia trie
+int comptageMot(patricia_node t);
+
+// Return a word list from patricia trie
+wlist listeMots(patricia_node t);
+
+// Count NULL pointers in patricia trie
+int comptageNil(patricia_node t);
+
+// Return average patricia trie depth
+int profondeurMoyenne(patricia_node t);
+
+// Count word prefix from word
+int prefixe(patricia_node t ,char* mot);
+
+// Remove word from hybrid trie
+patricia_node suppression(patricia_node t ,char* mot );
+
+
+// Return height's patricia trie
+int hauteur(patricia_node t);
+
+
+
 
 #endif
 

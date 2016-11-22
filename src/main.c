37cf432929f0cb9207dd7daf0480ff28507c1699
cbyad <cb_mac@hhh.crans.org>
2016-11-22 20:00:32 +0100

    Nettoyage de code (conflit de fichier donc je le fais juste pour la synchro)

--- src/main.c
+++ src/main.c
@@ -1,24 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-
 #include "../include/patricia_trie.h"
 #include "../include/hybrid_trie.h"
+#include "../include/build_dict.h"
+#include "../include/word_list.h"
 
 int main() {
-  HTptr my_hybridTrie = NULL;
-  
-  /*************
-   * TEST
-   *************/
-  my_hybridTrie = insertHT(my_hybridTrie, "test");
-  insertHT(my_hybridTrie, "testament");
-  insertHT(my_hybridTrie, "testosterone");
-  insertHT(my_hybridTrie, "bon");
-  insertHT(my_hybridTrie, "bonjour");
-  removeHT(my_hybridTrie, "testamentj");
-  printf("%d\n", nbPrefixHT(my_hybridTrie, "te"));
-
-  freeHT(my_hybridTrie);
-  exit(0);
+    HTptr my_hybridTrie = NULL;
+    char* path = "example/basic";
+    wlist basic = read_dict(path);
+    for (wlist tmp = basic->next; tmp != basic; tmp = tmp->next)
+        my_hybridTrie = insertHT(my_hybridTrie, tmp->word);
+    printf("words: %d\n", countwHT(my_hybridTrie));
+    printf("nbNULL: %d\n", nbNULLHT(my_hybridTrie));
+    printf("heightHT: %d\n", heightHT(my_hybridTrie));
+    char* search = "professeur";
+    printf("search %s: %d\n", search, searchHT(my_hybridTrie, search));
+    freeHT(my_hybridTrie);
+    exit(0);
 }
